0183f3aef718694797e37851ede84f4b3969ad0d
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2020-01-31 09:19:14 -0300

    Inicia navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav } from './Navbar.style'
+
+const Navbar = () => (
+    <Nav>
+        <img src="./img/nav-brand.png" />
+    </ Nav>
+)
+
+export default Navbar
---
+++ src/components/Navbar/Navbar.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Nav = styled.header`
+    background: #334A52;
+    padding: 8px 48px
+`
+
