473040b26471f67209d85d63097a34c5d0559555
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2020-02-04 15:12:09 -0300

    Adiciona navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Nav } from './Navbar.style'
+import { NavBody, Nav, NavList, NavItem, NavLink } from './Navbar.style'
 
-const Navbar = () => (
-  <Nav>
+const navItens = [{'text':'comprar'}, {'text':'perfil'}, {'text':'carrinho'}]
+const renderNavItem = () => (
+  navItens.map((elm) => (
+    <NavItem>
+      <NavLink>{elm.text}</NavLink>
+    </NavItem>
+  ))
+) 
+
+const Navbar = () => (  
+  <NavBody>
     <img src="./img/nav-brand.png" />
-  </ Nav>
+
+    <Nav>
+      <NavList>
+        {renderNavItem()}
+      </NavList>
+    </Nav>
+  </ NavBody>
 )
 
 export default Navbar
--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components'
 
-export const Nav = styled.header`
+export const NavBody = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background: #334A52;
-  padding: 8px 48px
+  padding: 8px 48px;
+`
+
+export const Nav = styled.nav`
+  color: #F7F7EB;
+`
+
+export const NavList = styled.ul`
+  display: flex;
+`
+
+export const NavItem = styled.li`
+  margin-right: 16px;
+`
+
+export const NavLink = styled.a`
+  cursor: pointer;
+  :hover {
+    color: #96AFB9;
+  }
 `
 
