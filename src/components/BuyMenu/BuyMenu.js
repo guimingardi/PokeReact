03cf2cf14c28f242a72c72edfa68010d85f6d1bb
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2020-02-05 11:40:16 -0300

    inicia captura de dados da api

--- src/components/BuyMenu/BuyMenu.js
+++ src/components/BuyMenu/BuyMenu.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Wrapper } from './BuyMenu.style'
 
-const BuyMenu = () => (
-  <Wrapper></Wrapper>
+const BuyMenu = ({ name, type, price }) => (
+  <Wrapper>
+
+<h1>{name}</h1>
+<h2>{type}</h2>
+<h3>{price}</h3>
+
+  </Wrapper>
 )
 
 export default BuyMenu
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
@@ -6,25 +6,45 @@ import BuyMenu from '../components/BuyMenu'
 import Footer from '../components/Footer'
 
 const App = () => {
+  const [pokemons, setPokemons] = useState(null)
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=20')
+      .then((res) => {
+        return res.json()
+      })
+      .then((res) => {
+        Promise.all(res.results.map(pokemonsObj => {
+          return fetch(pokemonsObj.url)
+            .then((pokemonsUrl) => pokemonsUrl.json())
+        })).then((valuePokemonsUrl) => {
+          console.log(valuePokemonsUrl)
+          setPokemons(valuePokemonsUrl)
+        })
+      })
+  }, [])
+
+
+
   return (
-      <Layout>
-        <Navbar />
-        <Main>
-          <BuyMenu />
+    <Layout>
+      <Navbar />
+      <Main>
 
+        {pokemons && pokemons.map(item => (
+              <BuyMenu
+              key={item && item.name}
+              name={item && item.name}
+              type={item.types[0].type.name}
+              price={item.base_experience}
+              />
+              )
+          )
+        }
 
+      </Main>
+      <Footer />
+    </Layout>
 
-
-
-
-
-
-
-
-
-        </Main>
-        <Footer />
-      </Layout>
   )
 }
 
